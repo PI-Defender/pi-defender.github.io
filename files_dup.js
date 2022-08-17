7e997b1a1bb39606394b5037623e96c9c18a9e18
Nikj-Fr <jallet.nicolas16@gmail.com>
2022-08-17 08:46:19 +0200

    Released documentation

---
+++ files_dup.js
@@ -0,0 +1,5 @@
+var files_dup =
+[
+    [ "_doxygen", "dir_09766e31dd8beaec91e22dd5f3c4a784.html", null ],
+    [ "Sources", "dir_937900b87e0c7a5fa01190c395fb83f7.html", "dir_937900b87e0c7a5fa01190c395fb83f7" ]
+];
---
+++ navtreedata.js
@@ -0,0 +1,89 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PI-Defender", "index.html", [
+    [ "Introduction", "index.html", [
+      [ "How does it works ?", "index.html#autotoc_md1", [
+        [ "Driver Kernel", "index.html#autotoc_md2", [
+          [ "Handle Rights", "index.html#autotoc_md3", null ],
+          [ "Configuration", "index.html#autotoc_md4", [
+            [ "Whitelist", "index.html#autotoc_md5", null ],
+            [ "Cache", "index.html#autotoc_md6", null ],
+            [ "Communication Port", "index.html#autotoc_md7", null ],
+            [ "Number of clients", "index.html#autotoc_md8", null ]
+          ] ]
+        ] ],
+        [ "Service User-Mode", "index.html#autotoc_md9", [
+          [ "Verify Signature", "index.html#autotoc_md10", null ],
+          [ "Configuration", "index.html#autotoc_md11", [
+            [ "Listener Thread", "index.html#autotoc_md12", null ],
+            [ "Workers Threads", "index.html#autotoc_md13", null ]
+          ] ]
+        ] ]
+      ] ],
+      [ "Maintainers/Authors", "index.html#autotoc_md14", null ],
+      [ "Credits", "index.html#autotoc_md15", null ],
+      [ "Licensing", "index.html#autotoc_md16", null ]
+    ] ],
+    [ "Demonstration", "md__doxygen__d_e_m_o_n_s_t_r_a_t_i_o_n.html", null ],
+    [ "Getting started", "md__doxygen__g_e_t_t_i_n_g__s_t_a_r_t_e_d.html", [
+      [ "Tested on", "md__doxygen__g_e_t_t_i_n_g__s_t_a_r_t_e_d.html#autotoc_md19", null ],
+      [ "Building", "md__doxygen__g_e_t_t_i_n_g__s_t_a_r_t_e_d.html#autotoc_md20", null ],
+      [ "Installation", "md__doxygen__g_e_t_t_i_n_g__s_t_a_r_t_e_d.html#autotoc_md21", null ],
+      [ "Start / Usage", "md__doxygen__g_e_t_t_i_n_g__s_t_a_r_t_e_d.html#autotoc_md22", null ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_k___cache_8cpp.html",
+"class_u___h_e_l_p_e_r.html#a7c6cd3e238d3e7965faa1bf34cf818f1"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
